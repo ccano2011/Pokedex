14e8334fc96bf90b7321eae8a2ce1aa20399c71a
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-09-22 00:01:23 -0400

    Solved the preventDefault issue

--- app.js
+++ app.js
@@ -4,15 +4,16 @@
 const submitButton = document.querySelector("#submit");
 const pokemonSearch = document.querySelector("#pokemonSearch");
 
-//I could not figure out how to prevent the default behavior of clicking the button.
-submitButton.addEventListener("click", userInput, async (event) => {
-  event.preventDefault();
-})
+//Janice & I tried to figure out how to prevent the default behavior of clicking the button.
+//Finally figured it out after looking at the Dog-code-along. 
+//The part that's commented out is what I had originally tried w/ Janice
+submitButton.addEventListener("click", userInput)//, async (event) => {
+// event.preventDefault();})
 
 
-async function userInput() {
+async function userInput(e) {
+  e.preventDefault()
   try {   //stopwatch @ 15:00
-
     let userInput = pokemonSearch.value;
     console.log(userInput)
     const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://pokeapi.co/api/v2/pokemon/${userInput}`)
