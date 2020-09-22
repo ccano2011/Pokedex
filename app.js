e4b79a8541c5d5070a5210fda46429d5a7fcdfd1
Chris Cano <ccano2011@gmail.com>
2020-09-22 14:53:39 -0400

    Latest commit for troubleshooting

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 // Code used as reference (w3school):
-// userInput()
+// "userInput()
 // button.addEventListener('click', async (event) => {
 //   // Prevent form from doing it's thing.
-//   event.preventDefault();
+//   event.preventDefault();"
 
-const APIdomain = 'https://pokeapi.co/api/v2/pokemon/';
+// const APIdomain = 'https://pokeapi.co/api/v2/pokemon/';
 const submitButton = document.querySelector("#submit");
 const pokemonSearch = document.querySelector("#pokemonSearch");
-const pokemonName = document.querySelector(".top-of-page");
+const topOfPage = document.querySelector(".top-of-page");
+
+submitButton.addEventListener("click", userInput)//, async (event) => {
+// event.preventDefault();})
+
+//CORS Work around: https://cors-anywhere.herokuapp.com/
+
 const pokedexNumber = document.querySelector(".pokedexID");
 const spritePicture = document.querySelector(".picture");
 const pokemonInfo = document.querySelector(".info");
 const pokemonType = document.querySelector(".type");
 const baseStats = document.querySelector(".base-stats");
 
-//Janice & I tried to figure out how to prevent the default behavior of clicking the button.
-//Finally figured it out after looking at the Dog-code-along. 
-//The part that's commented out is what I had originally tried w/ Janice
-submitButton.addEventListener("click", userInput)//, async (event) => {
-// event.preventDefault();})
-
-
 async function userInput(e) {
   e.preventDefault()
-  try {   //stopwatch @ 15:00
+  try {
     let userInput = pokemonSearch.value;
-    console.log(userInput)
-    const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://pokeapi.co/api/v2/pokemon/${userInput}`)
+    // console.log(userInput)
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
     console.log(response)
-    //printList(response.data.Search)
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
 userInput()
 
+// function renderList() {
+//   const pokemonName = document.createElement('h1')  //;
+//   let name = data.species.name
+//   pokemonName.innerHTML = `${name}`
+//   document.querySelector('.topOfPage').appendChild(name)
+// }
