13439648bd78e496db391190d7eb85c9b3175722
Chris Cano <ccano2011@gmail.com>
2020-09-22 10:46:14 -0400

    New variables added in JS

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-// const APIdomain = 'https://pokeapi.co/api/v2/pokemon/';
+// Code used as reference (w3school):
+// userInput()
+// button.addEventListener('click', async (event) => {
+//   // Prevent form from doing it's thing.
+//   event.preventDefault();
 
-
+const APIdomain = 'https://pokeapi.co/api/v2/pokemon/';
 const submitButton = document.querySelector("#submit");
 const pokemonSearch = document.querySelector("#pokemonSearch");
+const pokemonName = document.querySelector(".top-of-page");
+const pokedexNumber = document.querySelector(".pokedexID");
+const spritePicture = document.querySelector(".picture");
+const pokemonInfo = document.querySelector(".info");
+const pokemonType = document.querySelector(".type");
+const baseStats = document.querySelector(".base-stats");
 
 //Janice & I tried to figure out how to prevent the default behavior of clicking the button.
 //Finally figured it out after looking at the Dog-code-along. 
@@ -18,11 +28,10 @@ async function userInput(e) {
     console.log(userInput)
     const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://pokeapi.co/api/v2/pokemon/${userInput}`)
     console.log(response)
+    //printList(response.data.Search)
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
-// userInput()
-// button.addEventListener('click', async (event) => {
-//   // Prevent form from doing it's thing.
-//   event.preventDefault();
+userInput()
+
