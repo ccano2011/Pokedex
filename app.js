a1eb53e39bba6085c2fa520fe6af8295dc21eada
Chris Cano <ccano2011@gmail.com>
2020-09-21 21:20:18 -0400

    Fixed <script> location within the HTML & added more JS

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
-const APIdomain = 'https://pokeapi.co/api/v2/';
+// const APIdomain = 'https://pokeapi.co/api/v2/pokemon/';
+
 
 const submitButton = document.querySelector("#submit");
-const pokemonSearch = 
+const pokemonSearch = document.querySelector("#pokemonSearch");
+submitButton.addEventListener("click", (event) => {
+  userInput;
+  event.preventDefault()
+});
+
+
+async function userInput() {
+  try {   //stopwatch @ 15:00
+
+    let userInput = pokemonSearch.value;
+    console.log(userInput)
+    const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://pokeapi.co/api/v2/pokemon/${userInput}`)
+    console.log(response)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+userInput()
+
+
+
+// ${ APIdomain }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
