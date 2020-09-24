cd419c1bb3740e9600b5549515a637585e802109
Chris Cano <ccano2011@gmail.com>
2020-09-23 20:01:43 -0400

    Basic JS Completed

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const topOfPage = document.querySelector(".topOfPage");
 
 submitButton.addEventListener("click", userInput)
 
-//CORS Work around: https://cors-anywhere.herokuapp.com/
+//Potential CORS Work around: https://cors-anywhere.herokuapp.com/
+
 
 async function userInput(e) {
   e.preventDefault()
   try {
-    let userInput = pokemonSearch.value;
+    let userInput = pokemonSearch.value.toLowerCase();
     // console.log(userInput)
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
-    console.log(response.data)
+    // console.log(response.data)
     renderList(response.data)
+
   } catch (error) {
     console.log(`Error: ${error}`)
   }
@@ -23,13 +25,47 @@ async function userInput(e) {
 
 const renderList = pokemon => {
   const pokemonName = document.querySelector('#name');
-  pokemonName.innerHTML = pokemon.name;
+  pokemonName.innerHTML = pokemon.name.toUpperCase();
 
   const sprite = document.querySelector('#spritePic');
   sprite.setAttribute('src', pokemon.sprites.front_default);
 
   const pokedexID = document.querySelector("#idNum");
-  pokedexID.innerHTML = pokemon.id;
-
-
+  pokedexID.innerHTML = pokemon.id
+  pokemonDescription(pokemon.id)
 }
+// const pokedexID = document.querySelector("#idNum").value
+// console.log(pokemonID)
+
+function pokeInfo(string) {
+  let pokeInfo = document.querySelector('#flavorText');
+  pokeInfo.innerHTML = textEntries
+}
+
+
+async function pokemonDescription(id) {
+  try {
+    const returnId = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
+    let flavorTextPacket = returnId.data.flavor_text_entries
+    // console.log(flavorTextPacket)
+    textEntries = getflavorText(flavorTextPacket)
+    // console.log(textEntries)
+    pokeInfo(textEntries)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+
+// mac's help:
+
+function getflavorText(pokedexEntry) {
+  for (let i = 0; i < pokedexEntry.length; i++) {
+    if (pokedexEntry[i].language.name === 'en') {
+      // console.log(pokedexEntry[i])
+      return pokedexEntry[i].flavor_text;
+    }
+  }
+}
+
+// work around jQuery loop that gave me the idea
+// for : ['language']['name'] == 'en'
